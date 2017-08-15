1c5de33f918f4bd93b50668e39f8ff3e966ed451
publicoder <gcoolmoon@gmail.com>
2017-08-15 16:27:27 -0500

    rest controllers added

---
+++ src/main/java/edu/mum/apms/rest/ReleaseRestController.java
@@ -0,0 +1,37 @@
+package edu.mum.apms.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.mum.apms.model.Release;
+import edu.mum.apms.model.Sprint;
+import edu.mum.apms.service.ReleaseService;
+
+@RestController
+public class ReleaseRestController {
+
+
+	@Autowired
+	ReleaseService releaseService;
+	
+	
+	@RequestMapping("/restreleases")
+    public List<Release> releases() {
+        return releaseService.getAll();
+    }
+	
+	@RequestMapping("/restrelease/{id}")
+    public Release release(@PathVariable("id") int id) {
+        return releaseService.get(id);
+    }
+	
+	@RequestMapping("/restsprintbyrelease/{id}")
+    public List<Sprint> sprintByRelease(@PathVariable("id") int id) {
+		
+        return releaseService.get(id).getSprint();
+    }
+}
